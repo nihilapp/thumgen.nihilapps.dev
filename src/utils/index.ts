18dc240d590f461fc308fa0f20ad0c6ae9522e35
NIHILncunia <nihil_ncunia@naver.com>
2024-10-15 03:35:56 +0900

    2024 1015 까먹은 파일 삭제

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,1 @@
-export { Api } from './axios';
 export { setMeta } from './setMeta';
-export { DB } from './prisma';
-export { cx } from './classnames';
-export { localStorageEffect } from './localstorageEffect';
