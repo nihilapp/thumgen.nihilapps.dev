6700312d3d7370703f40bd9996df46ece9ed6243
NIHILncunia <nihil_ncunia@naver.com>
2024-10-15 03:43:04 +0900

    2024 1015 까먹은 파일 삭제

--- src/utils/tools/index.ts
+++ src/utils/tools/index.ts
@@ -1,19 +1,15 @@
 import { Common } from '@/src/utils/tools/common';
 import { Calendar } from '@/src/utils/tools/calendar';
-import { ClientCookie } from '@/src/utils/tools/cookie';
 
 export { Common };
 export { Calendar };
-export { ClientCookie };
 
 interface Tools {
   common: Common;
   calendar: Calendar;
-  cookie: ClientCookie;
 }
 
 export const tools: Tools = {
   common: new Common(),
   calendar: new Calendar(),
-  cookie: new ClientCookie(),
 };
